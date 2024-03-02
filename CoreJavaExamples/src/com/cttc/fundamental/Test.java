bcd37319acbd9339b6fc5e35aa87d1a2888f7bb2
cttc <cttc@DESKTOP-7I78IDR>
2024-03-01 19:53:02 -0800

    tt

--- CoreJavaExamples/src/com/cttc/fundamental/Test.java
+++ CoreJavaExamples/src/com/cttc/fundamental/Test.java
@@ -3,7 +3,7 @@ package com.cttc.fundamental;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Cttc..");
+		System.out.println("Hello Cttc..000");
 		byte x = 127;
 		byte y = x++;
 		System.out.println(x + " " + y);
